441360172b4b276a8951f161d199db3ececd1a2c
fengxiaohui <fengxiaohui@tetras.ai>
2026-04-13 16:46:47 +0800

    update .gitignore and App.tsx

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { Layout } from './components/layout';
+import { Layout } from './components/Layout';
 import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
     <>
-      <Layout />
+      <Layout>{null}</Layout>
       <Toaster />
     </>
   );
